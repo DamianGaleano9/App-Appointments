2d0f437479b51e968fbb2dd9a333cc8e0d4aa4f1
Damian Galeano <dj.damian.b@hotmail.com>
2018-08-02 21:18:53 -0500

    Getting result

--- js/script.js
+++ js/script.js
@@ -1,34 +1,75 @@
-
-
-
-
+var users = [
+    {
+        name: 'Bob',
+        gender: 'F',
+        hobby: 'Video Games',
+        avatar: 'avatar1.png'
+    },
+    {
+        name: 'Jerry',
+        gender: 'F',
+        hobby: 'Video Games',
+        avatar: 'avatar2.png'
+    },
+    {
+        name: 'Sue',
+        gender: 'M',
+        hobby: 'Student',
+        avatar: 'avatar3.png'
+    },
+    {
+        name: 'Adrien',
+        gender: 'M',
+        hobby: 'Singer',
+        avatar: 'avatar4.png'
+    },
+    {
+        name: 'Paul',
+        gender: 'F',
+        hobby: 'RockStar',
+        avatar: 'avatar5.png'
+    },
+    {
+        name: 'Michael',
+        gender: 'F',
+        hobby: 'Web Desing',
+        avatar: 'avatar6.png'
+    },
+    {
+        name: 'Scoot',
+        gender: 'F',
+        hobby: 'Programming',
+        avatar: 'avatar7.png'
+    },
+];
 
 
 window.addEventListener('load', function() {
 
 
-    console.log("Successfully loaded page");
-
-    var results = document.getElementById('result');
-
+    var results = document.getElementById('results');
     var searchBtn = document.getElementById('searchBtn');
         searchBtn.addEventListener('click', function() {
 
         //Get Hobby
 
-        var hobieField = document.getElementById('hobby');
-        var hobby = hobieField.value;
+        var hobbyField = document.getElementById('hobby');
+        var hobby = hobbyField.value;
 
         //Get Gender
 
         var genderField = document.getElementById('gender');
         var selected = genderField.selectedIndex;
         var gender = genderField.options[selected].value;
-
         console.log(gender);
 
+        var resultsHTML = '';
+        var numUsers = users.length;
 
+        for(var i = 0; i < numUsers; i++) {
+            resulstHTML = resultsHTML + ' ' + users[i].name;
+        }
 
-        results.innerHTML = "Hello"
+            results.innerHTML = resultsHTML;
         });
 });
